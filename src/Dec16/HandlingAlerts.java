2cac86d28c863da4a8e68aea02734426860b77d1
HP <HP@192.168.0.5>
2019-12-19 14:27:34 +0530

    Dec 16, Handling Alerts

--- src/Dec16/HandlingAlerts.java
+++ src/Dec16/HandlingAlerts.java
@@ -6,20 +6,27 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HandlingAlerts {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		WebDriver drFirefox=new FirefoxDriver();
-		drFirefox.get("");
-		drFirefox.findElement(By.xpath("")).click();
-
+		drFirefox.get("file:///G:\\promp.html");
+		Thread.sleep(5000);
+		
+		drFirefox.findElement(By.xpath("/html/body/fieldset/button")).click();
+		Thread.sleep(5000);
+	
 		String alertText=drFirefox.switchTo().alert().getText();
 		System.out.println(alertText);
-
+		Thread.sleep(5000);
 		drFirefox.switchTo().alert().sendKeys("KGDPrasad");
+		Thread.sleep(5000);
+	
 		drFirefox.switchTo().alert().accept();
-
+		Thread.sleep(5000);
+	
 		String msg=drFirefox.findElement(By.id("promptdemo")).getText();
 		System.out.println(msg);
-
+	
+		Thread.sleep(5000);
 		drFirefox.quit();
 
 	}
